310e2b03fdcab53a417b90afffabcd8f95070998
orlandoquintero45 <orlandoquintero45@gmail.com>
2020-04-01 00:15:59 -0500

    project start

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (isset($_SESSION['autentificado']) != "VERDADERO") 
+{ 
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) 
+	{
+		$uri = 'https://';
+	}
+	else
+	{
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	echo "<script type='text/javascript'>window.location='".$uri."/php/view/login.php'</script>"; 
+	exit(); 
+}

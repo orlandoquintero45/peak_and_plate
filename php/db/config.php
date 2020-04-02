98a026ea048f48c3af94aada55433edd012c531c
orlandoquintero45 <orlandoquintero45@gmail.com>
2020-04-02 02:23:04 -0500

    global variables

---
+++ php/db/config.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!isset($_SESSION)) 
+{ 		
+	session_start();
+}
+// configuration variables
+define("server", "127.0.0.1");
+define("database", "");
+define("user_", "root");
+define("pass", "");
+define("port", "3306");
+
+
+?>
